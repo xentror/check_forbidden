dd6eaf5a8a549e3ab563b62236930a739c75e9ab
Thibault Vivies <thibault.vivies@intersec.com>
2020-01-02 14:57:53 +0100

    check_forbidden: Fix many bugs and change -f options.

--- README.md
+++ README.md
@@ -14,17 +14,20 @@ A simple shell script which checks that you only use authorized functions in a c
 ./check_forbid.sh ./binary_name
 ```
 The script will ask you how many authorized functions you have and you will have to write their name, without paranthesis.
-  
+
  *e.g: for ```malloc()``` function you just have to type ```malloc```*
-        
+
 ### Functions list file (*-f*): 
 ```
 ./check_forbid.sh ./binary_name -f filename:
 ```
-The script will take a file with a list of authorized functions, all writting in one line and following this pattern:
-  
-        -[function_name]-[function_name]...etc
-       
-## KNOWN BUGS 
+The script will take a file with a list of authorized functions, each function name on a line
+like this pattern:
+
+        function_name1\n
+        function_name2\n
+        [...]
+
+## KNOWN BUGS
 
 This script use ```ltrace``` so for example functions like ```printf``` can be replaced by ```puts``` or ```putchar``` by the compiler so be careful and check your code by hand too. ;).
--- check_forbid.sh
+++ check_forbid.sh
@@ -1,32 +1,52 @@
 #! /bin/bash
 
-if [ $# -eq 3 ] && [ "$2" = "-f" ] && [ -r $3 ];then
-    args=`cat $3`
+[ $# -lt 2 ] && echo "usage: ./check_forbid.sh [-f file] binary" && exit 1
+
+args=''
+if  [ "$1" = "-f" ];
+then
+    if [ $# -ne 3 ] || [ ! -r $2 ];
+    then
+        echo "usage: ./check_forbid.sh [-f file] binary" && exit 1
+    fi
+
+    for function_name in `cat $2`;
+    do
+        args=$args-$function_name
+    done
+    binary=$3
+    options='-e'
 else
     echo -n "Combien de fonctions sont autorisées ? "
     read nb_func
-    args=''
 
     while [ $nb_func -gt 0 ];do
         read arg
         args=$args-$arg
         nb_func=$((nb_func - 1))
     done
+
+    binary=$1
 fi
 
-options=''
-[ "$args" != "" ] && options='-e'
+[ ! -x $binary ] && echo "cannot execute $binary file." && exit 1
 
-ltrace $options $args $1 2> count 1> delete
-ltrace -c $options $args $1 2> log 1> delete
+ltrace -c $options $args $binary 2> log 1> NULL
 
-nb_forbid=$((`wc -l count | cut -d ' ' -f 1`-1))
-rm -f delete count
+nb_forbid=$(wc -l log | sed s/[^0-9]*//g)
+nb_forbid=$(($nb_forbid-4))
+
 [ $nb_forbid -eq 0 ] && echo " FELICITATIONS: pas de fonction interdite utilisée" && exit 0
 
-echo -n "$nb_forbid fonctions interdites utilisées. Voulez-vous voir les logs (y/n) ? "
-read choice
-[ "$choice" = "n" ] && echo "Vous pouvez voir les logs dans le fichier log à la racine du programme.
-BYE BYE ;)" && exit 
+echo -n "$nb_forbid fonctions interdites utilisées. Voulez-vous voir les logs (Y/n) ? "
 
-cat log
+read choice
+if [ "$choice" != "n" ];
+then
+    cat log
+else
+    echo "Vous pouvez voir les logs dans le fichier log à la racine du
+    programme.  BYE BYE ;)"
+fi
+
+exit
